ff0fbaf4ed59543402ab30fde8479202b0e9ef51
Alex <alexander.ogier@gmail.com>
2021-07-08 00:43:08 +1200

    day 4 5 6

--- day5/part1.js
+++ day5/part1.js
@@ -1,4 +1,15 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(p => parseInt(p));
 
+var pc = 0;
+var steps = 0;
+
+while (pc >= 0 && pc < input.length) {
+    var jv = input[pc];
+    input[pc]++;
+    pc += jv;
+    steps++;
+}
+
+console.log(steps);
