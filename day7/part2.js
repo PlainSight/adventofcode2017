667bd22f5349fb1d4d80ff6b8fde4f0e932b1ec7
Alex <alexander.ogier@gmail.com>
2021-07-09 00:13:18 +1200

    day 7 and 8

--- day7/part2.js
+++ day7/part2.js
@@ -2,3 +2,35 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var regex = /([a-z]+) \((\d+)\)(?: -> (.+))?/;
+
+var programs = {};
+
+for(var i = 0; i < input.length; i++) {
+    var result = regex.exec(input[i]);
+    var name = result[1];
+    var weight = parseInt(result[2]);
+    var children = [];
+    if (result[3]) {
+        children = result[3].split(', ');
+    }
+    programs[name] = {weight: weight, children: children };
+}
+
+function calcWeight(p) {
+    return p.weight + p.children.map(c => calcWeight(programs[c])).reduce((a, b) => a+b, 0);
+}
+
+for(var p in programs) {
+    var prog = programs[p];
+    if (prog.children.length) {
+        var weights = prog.children.map(c => calcWeight(programs[c]));
+        var baseWeight = weights[0];
+        var badWeight = weights.filter(w => w != baseWeight);
+        if (badWeight.length > 0) {
+            console.log(p, prog, weights);
+        }
+    }
+}
+
+// 802 - via using brain
--- day8/part1.js
+++ day8/part1.js
@@ -2,3 +2,26 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var regex = /([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) (<|>|>=|<=|==|!=) (-?\d+)/;
+
+var registers = {};
+
+for(var i = 0; i < input.length; i++) {
+    var inst = regex.exec(input[i]);
+    var reg = inst[1];
+    var iod = inst[2];
+    var amount = parseInt(inst[3]);
+    var condreg = inst[4];
+    var condop = inst[5];
+    var condval = parseInt(inst[6]);
+
+    var condRegValue = registers[condreg] || 0;
+
+    var condTrue = eval(condRegValue + condop + condval);
+
+    if (condTrue) {
+        registers[reg] = (registers[reg] || 0) + (iod == 'inc' ? amount : -amount);
+    }
+}
+
+console.log(Math.max(...Object.values(registers)));
