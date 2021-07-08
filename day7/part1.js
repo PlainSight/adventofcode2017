dc16afa16091559fb9fedafe2b6446be0323e406
Alex <alexander.ogier@gmail.com>
2021-07-09 00:13:18 +1200

    day 7 and 8

--- day7/part1.js
+++ day7/part1.js
@@ -2,3 +2,21 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var regex = /([a-z]+) \((\d+)\)(?: -> (.+))?/;
+
+var programs = [];
+var allChildren = [];
+
+for(var i = 0; i < input.length; i++) {
+    var result = regex.exec(input[i]);
+    var name = result[1];
+    var weight = parseInt(result[2]);
+    var children = [];
+    if (result[3]) {
+        children = result[3].split(', ');
+    }
+    programs.push({ name: name, weight: weight, children: children });
+    allChildren.push(...children);
+}
+
+console.log(programs.filter(p => !allChildren.includes(p.name)));
