f91032d46031287e55f5cae21ee026734c62a238
Alex <alexander.ogier@gmail.com>
2021-07-10 20:59:32 +1200

    day 9 10 11

--- day10/part2.js
+++ day10/part2.js
@@ -1,4 +1,51 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8');
 
+var lengths = input.split('').map(n => n.charCodeAt(0));
+
+lengths.push(...[17, 31, 73, 47, 23]);
+
+var list = [];
+
+for (var i = 0; i < 256; i++) {
+    list.push(i);
+}
+
+var curPos = 0;
+var skipSize = 0;
+
+function reverse(pos, len) {
+    var j = 0;
+    var toReverse = [];
+    for (var i = pos; j < len; i = ((i+1)%list.length)) {
+        j++;
+        toReverse.push(list[i]);
+    }
+    j = 0;
+    for (var i = pos; j < len; i = ((i+1)%list.length)) {
+        j++;
+        list[i] = toReverse.pop();
+    }
+}
+
+for (var r = 0; r < 64; r++) {
+    for(var i = 0; i < lengths.length; i++) {
+        reverse(curPos, lengths[i]);
+        curPos += (lengths[i]+skipSize);
+        curPos = curPos % list.length;
+        skipSize++;
+    }
+}
+
+var dense = list.reduce((pv, cv, ci) => { 
+    pv[Math.floor(ci/16)] = (pv[Math.floor(ci/16)] || 0) ^ cv;
+    return pv;
+}, {});
+
+console.log(Object.values(dense).map(d => {
+    var hex = d.toString(16);
+    return hex.length == 2 ? hex : '0' + hex;
+}).join(''));
+
+//toString(16)
--- day12/part2.js
+++ day12/part2.js
@@ -1,4 +1,53 @@
 var fs = require('fs');
+const { connect } = require('http2');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var nodes = {};
+
+function getRoot(node) {
+    if (node.parent) {
+        node.parent.value += node.value;
+        node.value = 0;
+        node.parent = getRoot(node.parent);
+        return node.parent;
+    }
+    return node;
+}
+
+for (var i = 0; i < input.length; i++) {
+    var vals = /(\d+) <-> (.+)/.exec(input[i]);
+
+    var element = parseInt(vals[1]);
+    var connections = vals[2].split(', ').map(p => parseInt(p));
+
+    if (!nodes[element]) {
+        nodes[element] = { name: element, parent: null, value: 1 };
+    }
+
+    connections.forEach(c => {
+        if (!nodes[c]) {
+            nodes[c] = { name: c, parent: null, value: 1 };
+        }
+    });
+
+    connections.forEach(c => {
+        if (element != c) {
+            //console.log('connecting', nodes[element], 'and', nodes[c]);
+            
+            if (getRoot(nodes[element]) != getRoot(nodes[c])) {
+                if (getRoot(nodes[element]).value >= getRoot(nodes[c]).value) {
+                    getRoot(nodes[c]).parent = getRoot(nodes[element]);
+                    getRoot(nodes[c]);
+                } else {
+                    getRoot(nodes[element]).parent = getRoot(nodes[c]);
+                    getRoot(nodes[element]);
+                }
+            }
+        }
+
+        //console.log(nodes);
+    });
+}
+
+console.log(Object.values(nodes).filter(n => n.value > 0).length);
