4c4a729223e0045cc851039518fae256044d2829
Alex <alexander.ogier@gmail.com>
2021-07-10 20:59:32 +1200

    day 9 10 11

--- day10/part1.js
+++ day10/part1.js
@@ -1,4 +1,37 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var lengths = fs.readFileSync('./input.txt', 'utf8').split(',').map(n => parseInt(n));
 
+var list = [];
+
+for (var i = 0; i < 256; i++) {
+    list.push(i);
+}
+
+var curPos = 0;
+var skipSize = 0;
+
+function reverse(pos, len) {
+    var j = 0;
+    var toReverse = [];
+    for (var i = pos; j < len; i = ((i+1)%list.length)) {
+        j++;
+        toReverse.push(list[i]);
+    }
+    j = 0;
+    for (var i = pos; j < len; i = ((i+1)%list.length)) {
+        j++;
+        list[i] = toReverse.pop();
+    }
+}
+
+for(var i = 0; i < lengths.length; i++) {
+    reverse(curPos, lengths[i]);
+    curPos += (lengths[i]+skipSize);
+    curPos = curPos % list.length;
+    skipSize++;
+}
+
+console.log(list);
+
+console.log(list[0]*list[1]);
--- day11/part2.js
+++ day11/part2.js
@@ -1,4 +1,43 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split(',');
+
+var x = 0;
+var y = 0;
+
+var furthestDistance = 0;
+
+for (var i = 0; i < input.length; i++) {
+    var dir = input[i];
+
+    switch (dir) {
+        case 'nw':
+            y--;
+            x--;
+            break;
+        case 'se':
+            x++;
+            y++;
+            break;
+        case 'n':
+            y--;
+            break;
+        case 's':
+            y++;
+            break;
+        case 'ne':
+            x++;
+            break;
+        case 'sw':
+            x--;
+            break;
+    }
+
+    var dist = Math.max(Math.abs(x), Math.abs(y));
+    if (dist > furthestDistance) {
+        furthestDistance = dist;
+    }
+}
+
+console.log(furthestDistance);
 
