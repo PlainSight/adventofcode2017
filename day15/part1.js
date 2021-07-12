b08616838c9ec91f1c237b5b6e5b0f2fa6a39784
Alex <alexander.ogier@gmail.com>
2021-07-13 00:03:39 +1200

    day 15 and 16

--- day15/part1.js
+++ day15/part1.js
@@ -1,4 +1,24 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+//input
+//Generator A starts with 277
+//Generator B starts with 349
 
+var lastA = 277;
+var lastB = 349;
+
+var mulA = 16807;
+var mulB = 48271;
+
+var div = 2147483647;
+
+var matches = 0;
+
+for (var i = 0; i < 40000000; i++) {
+    lastA = (lastA * mulA) % div;
+    lastB = (lastB * mulB) % div;
+    
+    matches += (lastA << 16 == lastB << 16) ? 1 : 0;
+}
+
+console.log(matches);
--- day15/part2.js
+++ day15/part2.js
@@ -1,4 +1,43 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+//input
+//Generator A starts with 277
+//Generator B starts with 349
 
+// test a: 65 b: 8921
+
+var lastA = 277;
+var lastB = 349;
+
+var mulA = 16807;
+var mulB = 48271;
+
+var count = 0;
+var mode = 'a';
+
+// a mul 4
+// b mul 8
+
+var div = 2147483647;
+
+var matches = 0;
+
+while (count < 5000000) {
+    if (mode == 'a') {
+        do {
+            lastA = (lastA * mulA) % div;
+        } while(lastA % 4 != 0)
+        mode = 'b';
+    } else {
+        do {
+            lastB = (lastB * mulB) % div;
+        } while(lastB % 8 != 0)
+        mode = 'a';
+
+        count++;
+        matches += (lastA << 16 == lastB << 16) ? 1 : 0;
+    }
+    
+}
+
+console.log(matches);
