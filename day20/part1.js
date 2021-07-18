8affc1aa471b4199d9c341118cc0defd7773c618
Alex <alexander.ogier@gmail.com>
2021-07-18 14:26:24 +1200

    day20

--- day20/part1.js
+++ day20/part1.js
@@ -2,3 +2,43 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var particles = [];
+
+for (var i = 0; i < input.length; i++) {
+    var ex = /p=<(.+),(.+),(.+)>, v=<(.+),(.+),(.+)>, a=<(.+),(.+),(.+)>/.exec(input[i]);
+
+    particles.push({ x: parseInt(ex[1]), y: parseInt(ex[2]), z: parseInt(ex[3]),
+                    dx: parseInt(ex[4]), dy: parseInt(ex[5]), dz: parseInt(ex[6]),
+                    ax: parseInt(ex[7]), ay: parseInt(ex[8]), az: parseInt(ex[9])
+                });
+}
+
+var closest = [];
+
+for (var i = 0; i < 100000; i++) {
+    var closestDist = Infinity;
+    var closestParticle = 0
+
+    for (var p = 0; p < particles.length; p++) {
+        var pp = particles[p];
+
+        pp.dx += pp.ax;
+        pp.dy += pp.ay;
+        pp.dz += pp.az;
+
+        pp.x += pp.dx;
+        pp.y += pp.dy;
+        pp.z += pp.dz;
+
+        var dist = Math.abs(pp.x) + Math.abs(pp.y) + Math.abs(pp.z);
+
+        if (dist < closestDist) {
+            closestDist = dist;
+            closestParticle = p;
+        }
+    }
+    closest.push(closestParticle);
+}
+
+console.log(closest.slice(99000));
+
--- day20/part2.js
+++ day20/part2.js
@@ -2,3 +2,45 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var particles = [];
+
+for (var i = 0; i < input.length; i++) {
+    var ex = /p=<(.+),(.+),(.+)>, v=<(.+),(.+),(.+)>, a=<(.+),(.+),(.+)>/.exec(input[i]);
+
+    particles.push({ x: parseInt(ex[1]), y: parseInt(ex[2]), z: parseInt(ex[3]),
+                    dx: parseInt(ex[4]), dy: parseInt(ex[5]), dz: parseInt(ex[6]),
+                    ax: parseInt(ex[7]), ay: parseInt(ex[8]), az: parseInt(ex[9]),
+                    alive: true
+                });
+}
+
+for (var i = 0; i < 10000; i++) {
+    var positions = {};
+
+    for (var p = 0; p < particles.length; p++) {
+        var pp = particles[p];
+        if (!pp.alive) {
+            continue;
+        }
+
+        pp.dx += pp.ax;
+        pp.dy += pp.ay;
+        pp.dz += pp.az;
+
+        pp.x += pp.dx;
+        pp.y += pp.dy;
+        pp.z += pp.dz;
+
+        var posString = pp.x+','+pp.y+','+pp.z;
+
+        if (positions[posString]) {
+            pp.alive = false;
+            positions[posString].parent.alive = false;
+        } else {
+            positions[posString] = { parent: pp };
+        }
+    }
+}
+
+console.log(particles.filter(p => p.alive).length);
+
