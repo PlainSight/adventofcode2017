19ef2cf4a6c71deece6a6b23dd101b0276e5f5d9
Alexander <alexander.ogier@trademe.co.nz>
2021-07-19 14:09:11 +1200

    day25

--- day25/part1.js
+++ day25/part1.js
@@ -1,4 +1,91 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var states = {
+    A: {
+        0: {
+            val: 1,
+            pos: 1,
+            nextState: 'B'
+        },
+        1: {
+            val: 0,
+            pos: -1,
+            nextState: 'F'
+        }
+    },
+    B: {
+        0: {
+            val: 0,
+            pos: 1,
+            nextState: 'C'
+        },
+        1: {
+            val: 0,
+            pos: 1,
+            nextState: 'D'
+        }
+    },
+    C: {
+        0: {
+            val: 1,
+            pos: -1,
+            nextState: 'D'
+        },
+        1: {
+            val: 1,
+            pos: 1,
+            nextState: 'E'
+        }
+    },
+    D: {
+        0: {
+            val: 0,
+            pos: -1,
+            nextState: 'E'
+        },
+        1: {
+            val: 0,
+            pos: -1,
+            nextState: 'D'
+        }
+    },
+    E: {
+        0: {
+            val: 0,
+            pos: 1,
+            nextState: 'A'
+        },
+        1: {
+            val: 1,
+            pos: 1,
+            nextState: 'C'
+        }
+    },
+    F: {
+        0: {
+            val: 1,
+            pos: -1,
+            nextState: 'A'
+        },
+        1: {
+            val: 1,
+            pos: 1,
+            nextState: 'A'
+        }
+    }
+};
 
+var tape = {};
+
+var state = 'A';
+var position = 0;
+
+for (var s = 0; s < 12794428; s++) {
+    var val = tape[position] || 0;
+    var nextStuff = states[state][val];
+    tape[position] = nextStuff.val;
+    position += nextStuff.pos;
+    state = nextStuff.nextState;
+}
+
+console.log(Object.values(tape).reduce((e, c) => e+c, 0));
